ea8b4f76da68715f34c544c2566c40af5ec96e4b
Simon GAUDIN <sgaudin@e3r7p13.42.fr>
2016-02-18 16:28:01 +0100

    ariable

--- main2.c
+++ main2.c
@@ -6,7 +6,7 @@
 /*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 15:31:07 by sgaudin           #+#    #+#             */
-/*   Updated: 2016/02/18 15:36:25 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/02/18 16:19:16 by sgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ int		main(void)
 	int ret_ft = 0;
 	int ret = 0;
 
-	ret_ft += ft_printf("{%3c}\n", 0);
-	ret += printf("{%3c}\n", 0);
+//	ret_ft += ft_printf("{%3c}\n", 0);
+	//ret += printf("{%3c}\n", 0);
 
-	printf("Printf retourne %d caracteres et ft_printf retourne %d caracteres", ret, ret_ft);
+//	ret_ft += ft_printf("{%5p}\n", NULL);
+//	ret += printf("{%5p}\n", NULL);
+	ret_ft += ft_printf("%#o\n", 42);
+	ret += printf("%#o\n", 42);
+	printf("\nPrintf retourne %d caracteres et ft_printf retourne %d caracteres", ret, ret_ft);
 	return (0);
 }
