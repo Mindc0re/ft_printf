a8c67810948c0aea182fc015c789602b77516fa4
Simon GAUDIN <sgaudin@e3r7p11.42.fr>
2016-03-07 16:57:33 +0100

    simon le fatiguer

--- main_flags.c
+++ main_flags.c
@@ -6,7 +6,7 @@
 /*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 17:26:09 by sgaudin           #+#    #+#             */
-/*   Updated: 2016/03/07 14:28:26 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/03/07 16:51:16 by sgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,25 @@ int		main(void)
 //	ret += printf("%4.15S\n", L"我是一只猫");
 //	ret_ft += ft_printf("% \n");
 //	ret += printf("% \n");
-	ret_ft = ft_printf("FT__%+o\n", 0);
-	ret = printf("NM__%+o\n", 0);
-	printf("PRINTF renvoie %d caracteres et FT_PRINTF renvoie %d caracteres\n", ret, ret_ft);
+/*
+	ret_ft = ft_printf("{%0+5d}\n", -42);
+	ret = printf("{%0+5d}\n", -42);
+	printf("PRINTF renvoie {%d} caracteres et FT_PRINTF renvoie {%d} caracteres\n", ret, ret_ft);
+
+	ret_ft = ft_printf("{%+10.5d}\n", 4242);
+	ret = printf("{%+10.5d}\n", 4242);
+	printf("PRINTF renvoie {%d} caracteres et FT_PRINTF renvoie {%d} caracteres\n", ret, ret_ft);
+
+	ret_ft = ft_printf("{%-+10.5d}\n", 4242);
+	ret = printf("{%-+10.5d}\n", 4242);
+	printf("PRINTF renvoie {%d} caracteres et FT_PRINTF renvoie {%d} caracteres\n", ret, ret_ft);
+*/
+	ret_ft = ft_printf("{%03.2d}\n", -1);
+	ret = printf("{%03.2d}\n", -1);
+	printf("PRINTF renvoie {%d} caracteres et FT_PRINTF renvoie {%d} caracteres\n", ret, ret_ft);
+/*
+	ret_ft = ft_printf("@moulitest: {%5.d}, {%5.0d}\n", 0, 0);
+	ret = printf("@moulitest: {%5.d}, {%5.0d}\n", 0, 0);
+	printf("PRINTF renvoie {%d} caracteres et FT_PRINTF renvoie {%d} caracteres\n", ret, ret_ft); */
 	return (0);
 }
--- src/calls/call_putnbr.c
+++ src/calls/call_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 17:56:48 by sgaudin           #+#    #+#             */
-/*   Updated: 2016/03/07 13:55:21 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/03/07 16:53:50 by sgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int		call_putnbr_part2(t_docker *data, int length, int prec, intmax_t result)
 	{
 		data->len += (length == 0 || (length == 1 && result < 0))
 		? longueur_nb(result, data) : 0;
+		printf("data->len = %d\n", data->len);
 		length = data->precision - longueur_nb(result, data);
 		length = result >= 0 ? length : length + 1;
 		prec = data->len;
@@ -64,9 +65,6 @@ int		call_putnbr(const char *str, va_list args, t_docker *data)
 	FT_INIT(int, prec, (data->more == 1 && result >= 0 ? ftp_putchar('+') : 0));
 	FT_INIT(int, len_nb, longueur_nb(result, data) + data->more + data->space);
 	result = signed_conversion(result, data);
-	if (result == 0 && data->precision == 0 && data->width == 0 &&
-		data->dot == 1 && data->less == 0)
-		return (0);
 	if (data->zero == 1 && result < 0 && str != NULL)
 		ftp_putchar('-');
 	if (data->less == 0 && data->width > 0)
@@ -74,12 +72,15 @@ int		call_putnbr(const char *str, va_list args, t_docker *data)
 		if (data->dot == 1)
 			if ((data->precision - len_nb + (result >= 0 ? 0 : 1)) > 0)
 				length -= data->precision - len_nb + (result >= 0 ? 0 : 1);
-		length += data->width - len_nb;
+		length += data->width - len_nb +
+			(result == 0 && data->precision == 0 && data->dot == 1 ? 1 : 0);
 		data->len = ft_add_spaces(length, data->len,
 								  (data->zero == 1 && data->dot == 0 ? '0' : ' '));
 	}
 	if (data->less == 0 && data->dot == 0 && result < 0 && data->zero == 0)
 		ftp_putchar('-');
+	if (result == 0 && data->precision == 0 && data->dot == 1 && data->less == 0)
+		return (0);
 	if (call_putnbr_part2(data, length, prec, result) == 0)
 		return (0);
 	return (ftp_putnbr(result, data));
