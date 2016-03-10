ce60b5437d2e72e738365d231e50ff9c4a6d4dd5
Dieuson VIRGILE <dvirgile@e2r7p11.42.fr>
2016-03-10 10:41:38 +0100

    test

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/05 11:23:26 by sgaudin           #+#    #+#             */
-/*   Updated: 2016/03/10 10:05:42 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/03/10 10:39:16 by dvirgile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # define FT_INIT(type,name,value)		type name = value
 # define TER(si,alors,sinon)			si ? alors : sinon
 # define MULTI(val_a, val_b, val_c)		val_a = val_b = val_c
+# include <stdarg.h>
 
 typedef struct		s_docker
 {
@@ -45,7 +46,6 @@ typedef struct		s_docker
 	};
 }					t_docker;
 
-# include <stdarg.h>
 # include "../libftprintf/libftprintf.h"
 # include "../libft/includes/libft.h"
 
@@ -84,5 +84,6 @@ void				detect_conversion(const char *str, t_docker *data);
 uintmax_t			unsigned_conversion(t_docker *data, va_list args);
 intmax_t			signed_conversion(t_docker *data, va_list args);
 int					ft_check_valid(const char *str, t_docker *data);
+int					detect(const char *s, t_docker *data);
 
 #endif
