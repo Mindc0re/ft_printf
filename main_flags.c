fde9656e34770322179b59df555b2b68dc043109
Dieuson VIRGILE <dvirgile@e2r7p11.42.fr>
2016-03-11 09:54:37 +0100

    fuck le main flag

--- main_flags.c
+++ main_flags.c
@@ -6,19 +6,22 @@
 /*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 11:57:21 by sgaudin           #+#    #+#             */
-/*   Updated: 2016/03/10 11:57:37 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/03/10 15:33:12 by dvirgile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 #include <stdio.h>
 #include <limits.h>
+#include <locale.h>
 
 int		main(void)
 {
 	int ret_ft = 0;
 	int ret = 0;
+	char *test;
 
+	test = setlocale(LC_ALL, "");
 //	int ret_ft = 0;
 //	int ret = 0;
 //	ret_ft = ft_printf("%");
@@ -35,8 +38,11 @@ int		main(void)
 	ret += printf("%+03d\n", 0);
 	ret_ft += ft_printf("% 03d\n", 0); // Probleme d'affichage
 	ret += printf("% 03d\n", 0); */
-//	ret_ft += ft_printf("%4.15S\n", L"我是一只猫");
-//	ret += printf("%4.15S\n", L"我是一只猫");
+	if (!test)
+		exit(EXIT_SUCCESS);
+	ret_ft += ft_printf("FT{%30S}", L"我是一只猫");
+	printf("\n");
+	ret += printf("NM{%30S}\n", L"我是一只猫");
 //	ret_ft += ft_printf("% \n");
 //	ret += printf("% \n");
 //	ft_printf("%hhld", "128");
@@ -46,16 +52,6 @@ int		main(void)
 	printf("\n");
 	ret = printf("NM__%%   %", "test");
 	printf("\nFT renvoi : %d, NM renvoi : %d\n", ret_ft, ret);*/
-
-	ret_ft = ft_printf("@main_ftprintf: {%###-#0000 33...12..#0+0d}\n", 256);
-	ret  = printf("@main_ftprintf: {%###-#0000 33...12..#0+0d}\n", 256);
-	printf("FT renvoi : %d, NM renvoi : %d\n", ret_ft, ret);
-	ret_ft = ft_printf("@main_ftprintf: {%####0000 33..1d}\n", 256);
-	ret = printf("@main_ftprintf: {%####0000 33..1d}\n", 256);
-	printf("FT renvoi : %d, NM renvoi : %d\n", ret_ft, ret);
-	ret_ft = ft_printf("{%5+d}\n", 42);
-	ret = printf("{%5+d}\n", 42);
-	printf("FT renvoi : %d, NM renvoi : %d\n", ret_ft, ret);
 //	ret_ft = ft_printf("% ");
 //	ret = printf("% ");
 //	printf("FT renvoi : %d, NM renvoi : %d\n", ret_ft, ret);
